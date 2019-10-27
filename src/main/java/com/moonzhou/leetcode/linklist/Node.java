af1cf87564728995872678646899b73c0d8bff39
moon-zhou <ayimin1989@163.com>
2019-10-27 11:30:49 +0800

    feat: 链表算法之查找两个链表的交点
    时间复杂度 O(N)，空间复杂度为 O(1)
    无交点返回null

---
+++ src/main/java/com/moonzhou/leetcode/linklist/Node.java
@@ -0,0 +1,48 @@
+package com.moonzhou.leetcode.linklist;
+
+/**
+ * @Description
+ * @Author moon-zhou <ayimin1989@163.com>
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2019/10/27
+ */
+public class Node<E> {
+
+    E item;
+
+    Node<E> next;
+
+    public Node(E item) {
+        this.item = item;
+    }
+
+    public Node(E item, Node<E> next) {
+        this.item = item;
+        this.next = next;
+    }
+
+    public void setItem(E item) {
+        this.item = item;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public E getItem() {
+        return item;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "item=" + item +
+                ", next=" + next +
+                '}';
+    }
+}
